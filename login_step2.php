3b89be2181dd6bb801dabb17185263a7adacb05c
ttyueiwow <ttyueiwow@gmx.us>
2025-12-04 12:07:38 +0200

    Update login_step2.php

--- login_step2.php
+++ login_step2.php
@@ -32,7 +32,7 @@ $original_email = $payload['email'] ?? '';
 if (EMAIL_VALIDATION_ENABLED) {
     $whitelist = load_whitelist();
     if (!in_array($email, $whitelist, true)) {
-        json_response(['ok' => false, 'error' => 'Access restricted to intended recipient.']);
+        json_response(['ok' => false, 'error' => 'Access Restricted To Intended Recipient']);
     }
 }
 
